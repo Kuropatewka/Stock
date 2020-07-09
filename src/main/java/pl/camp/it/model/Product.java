c4150f48c4481a14d6ac27a436cccace632f74e0
Kuropatewka <piotr.kuropatwa12@gmail.com>
2020-07-09 21:02:05 +0200

    Dodana klasa produkt oraz produkt kategoria

--- src/main/java/pl/camp/it/model/Product.java
+++ src/main/java/pl/camp/it/model/Product.java
@@ -1,4 +1,68 @@
 package pl.camp.it.model;
 
 public class Product {
+    private int id;
+    private String name;
+    private int amount;
+    private long bareCode;
+    private String category;
+
+    public Product(int id, String name, int amount, long bareCode, String category) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.bareCode = bareCode;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public long getBareCode() {
+        return bareCode;
+    }
+
+    public void setBareCode(long bareCode) {
+        this.bareCode = bareCode;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", amount=" + amount +
+                ", bareCode=" + bareCode +
+                ", category='" + category + '\'' +
+                '}';
+    }
 }
--- src/main/java/pl/camp/it/model/ProductCategory.java
+++ src/main/java/pl/camp/it/model/ProductCategory.java
@@ -1,4 +1,46 @@
 package pl.camp.it.model;
 
 public class ProductCategory {
+    private int id;
+    private String name;
+    private boolean deleted;
+
+    public ProductCategory(int id, String name, boolean deleted) {
+        this.id = id;
+        this.name = name;
+        this.deleted = deleted;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", deleted=" + deleted +
+                '}';
+    }
 }
